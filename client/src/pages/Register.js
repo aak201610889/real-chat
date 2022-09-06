7c6c9beb4c878572c29d74ec6ee7c96013e67836
ayham-kattan <aak201610885@gmail.com>
2022-09-06 00:51:48 -0700

    register-validate

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import logo from '../assets/Capture.PNG'
 import smLogo from '../assets/sm.PNG'
-import {} from 'rea'
+import {toast,ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 function Register() {
   const [user, setUser] = useState({name:'',email:"",password:"",confirmPassword:""})
 
@@ -11,11 +12,45 @@ function Register() {
   };
   const Submit = (e) =>  {
   //async
+  console.log(user)
    
     e.preventDefault();
     if(user.password!==user.confirmPassword){
+      toast.error('password not matching with confirm password.',{
+        position:"top-center",
+        autoClose:8000,
+        pauseOnHover:true,
+        draggable:true,
+        theme:"dark"
+      })
     
     }
+     if(user.name.length<3){
+      toast.error('username should be more than 3 charcteres.',{
+        position:"top-center",
+        autoClose:8000,
+        pauseOnHover:true,
+        draggable:true,
+        theme:"dark"
+      })}
+     if(user.email.length<3){
+      toast.error('email should be more than 3 charcteres.',{
+        position:"top-center",
+        autoClose:8000,
+        pauseOnHover:true,
+        draggable:true,
+        theme:"dark"
+      })
+     }
+     if(user.password.length<8){
+      toast.error('password should be more than 8 charcteres.',{
+        position:"top-center",
+        autoClose:8000,
+        pauseOnHover:true,
+        draggable:true,
+        theme:"dark"
+      })
+     }
     // try {
     //   await axios.post("/user/register", { ...user });
     //   localStorage.setItem("firstLogin", true);
@@ -27,6 +62,7 @@ function Register() {
 
   return (
     <div className="bg-[#2c2c2c] h-[100vh] "  >
+      <ToastContainer/>
       <div className="lg:flex md:flex block  justify-center items-center w-[100%] h-[100%]">
         <img src={smLogo} alt=""  className="w-[40%] m-auto pt-[10%] pb-[8%] lg:hidden md:hidden  block" />
 
@@ -36,11 +72,11 @@ function Register() {
       <div className=" bg-[#5ACA50] w-[45%] h-[100%]  rounded-2xl lg:flex md:flex items-center hidden"><img src={logo} alt="" /></div>
       <form action="" onSubmit={Submit} className="lg:w-[55%] md:w-[55%] sm:w-[80%] lg:bg-white md:bg-white h-[100%]   rounded-r-2xl  ">
      
-        <input type="text" className="block border-[#5aca50] border-[3px] py-[1%] m-auto mt-[15%] lg:w-[80%] md:w-[80%] w-[95%] lg:text-3xl md:text-2xl text-xl placeholder:text-[#7AC973]  capitalize pl-[3%]  h-[50px]  rounded-3xl" value={user.name}  onChange={changeInput} placeholder="name" name="name"/>
-        <input type="text" className="block border-[#5aca50] border-[3px] py-[1%] m-auto mt-[7%] lg:w-[80%] md:w-[80%] w-[95%] lg:text-3xl md:text-2xl text-xl placeholder:text-[#7AC973]  capitalize pl-[3%]  h-[50px] rounded-3xl" value={user.email}  onChange={changeInput} placeholder="email" name="email"/>
-        <input type="text" className="block border-[#5aca50] border-[3px] py-[1%] m-auto mt-[7%] lg:w-[80%] md:w-[80%] w-[95%] lg:text-3xl md:text-2xl text-xl placeholder:text-[#7AC973]  capitalize pl-[3%]  h-[50px] rounded-3xl" value={user.password}  onChange={changeInput} placeholder="password" name="password"/>
-        <input type="text" className="block border-[#5aca50] border-[3px] py-[1%] m-auto mt-[7%] lg:w-[80%] md:w-[80%] w-[95%] lg:text-3xl md:text-2xl text-xl placeholder:text-[#7AC973]  capitalize pl-[3%]  h-[50px] rounded-3xl" value={user.confirmPassword}  onChange={changeInput} placeholder="confirm password" name="confirmPassword"/>
-        <button  type="submit" className=" block capitalize pl-[3%] w-[50%] m-auto lg:h-[50px] md:h-[50px] h-[40px] rounded-3xl bg-[#5aca50] mt-[5%] lg:text-3xl md:text-2xl text-xl text-white mb-[15%]">Confirm</button>
+        <input type="text" className="block border-[#5aca50] border-[3px] py-[1%] m-auto mt-[15%] lg:w-[80%] md:w-[80%] w-[95%] lg:text-3xl md:text-2xl text-xl placeholder:text-[#7AC973]  capitalize outline-none pl-[3%]  h-[50px]  rounded-3xl" value={user.name}  onChange={changeInput} placeholder="name" name="name"/>
+        <input type="text" className="block border-[#5aca50] border-[3px] py-[1%] m-auto mt-[7%] lg:w-[80%] md:w-[80%] w-[95%] lg:text-3xl md:text-2xl text-xl placeholder:text-[#7AC973]  capitalize outline-none pl-[3%]  h-[50px] rounded-3xl" value={user.email}  onChange={changeInput} placeholder="email" name="email"/>
+        <input type="text" className="block border-[#5aca50] border-[3px] py-[1%] m-auto mt-[7%] lg:w-[80%] md:w-[80%] w-[95%] lg:text-3xl md:text-2xl text-xl placeholder:text-[#7AC973]  capitalize outline-none pl-[3%]  h-[50px] rounded-3xl" value={user.password}  onChange={changeInput} placeholder="password" name="password"/>
+        <input type="text" className="block border-[#5aca50] border-[3px] py-[1%] m-auto mt-[7%] lg:w-[80%] md:w-[80%] w-[95%] lg:text-3xl md:text-2xl text-xl placeholder:text-[#7AC973]  capitalize outline-none pl-[3%]  h-[50px] rounded-3xl" value={user.confirmPassword}  onChange={changeInput} placeholder="confirm password" name="confirmPassword"/>
+        <button  type="submit" className=" block capitalize outline-none pl-[3%] w-[50%] m-auto lg:h-[50px] md:h-[50px] h-[40px] rounded-3xl bg-[#5aca50] mt-[5%] lg:text-3xl md:text-2xl text-xl text-white mb-[15%]">Confirm</button>
       </form>
       </div>
  
